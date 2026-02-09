1a30f3e394a8375883ec0a706907dac08a003be0
Syed Zaman <zamanwebdev@gmail.com>
2026-02-09 16:47:38 +0600

    Day 4 Practice Done.

--- flask_course/app.py
+++ flask_course/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact',methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        # return f"Hello {username}, we received your message!"
+        return f"Hello {username}, your email is {email}"
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
