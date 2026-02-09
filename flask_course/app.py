e4243cfee032c3822e51fd5a870796964b045ed7
Syed Zaman <zamanwebdev@gmail.com>
2026-02-09 15:27:01 +0600

    Day 2 Practice Done.

--- flask_course/app.py
+++ flask_course/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to My Business Website!"
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return "This is About Page"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
